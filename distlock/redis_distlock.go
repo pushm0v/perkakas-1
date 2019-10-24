e111e9ee4fd1a35f7d5bf7f1b47b203382812527
Wahyu Said <bps.phi@gmail.com>
2019-10-24 17:04:28 +0700

    refactor redis distributed lock

--- distlock/redis_distlock.go
+++ distlock/redis_distlock.go
@@ -8,6 +8,17 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
+type DistLock struct {
+	Pool   *redis.Pool
+	Option Option
+}
+
+type Option struct {
+	RedisLockerTries      int
+	RedisLockerRetryDelay time.Duration
+	RedisLockerExpiry     time.Duration
+}
+
 const (
 	RedisLockerTries      = 3
 	RedisLockerRetryDelay = 2 * time.Second
@@ -17,17 +28,44 @@ const (
 var pools []redsync.Pool
 var reds *redsync.Redsync
 
-func SetCacheWithDistLock(pool *redis.Pool, key string, ttl interface{}, value interface{}) (err error) {
+func New(pool *redis.Pool, option ...Option) (distLock *DistLock) {
+	o := makeOption(option)
+	return &DistLock{
+		Pool:   pool,
+		Option: o,
+	}
+}
 
-	pools = []redsync.Pool{pool}
+func makeOption(option []Option) (o Option) {
+	o.RedisLockerTries = RedisLockerTries
+	o.RedisLockerRetryDelay = RedisLockerRetryDelay
+	o.RedisLockerExpiry = RedisLockerExpiry
+
+	for _, opt := range option {
+		if opt.RedisLockerTries != 0 {
+			o.RedisLockerTries = opt.RedisLockerTries
+		}
+		if opt.RedisLockerRetryDelay != 0 {
+			o.RedisLockerRetryDelay = opt.RedisLockerRetryDelay
+		}
+		if opt.RedisLockerExpiry != 0 {
+			o.RedisLockerExpiry = opt.RedisLockerExpiry
+		}
+	}
+	return o
+}
+
+func (d *DistLock) SetCacheWithDistLock(key string, ttl interface{}, value interface{}) (err error) {
+
+	pools = []redsync.Pool{d.Pool}
 	reds = redsync.New(pools)
 
 	lockKey := fmt.Sprintf("lock-key-%s", key)
 
 	mtx := reds.NewMutex(lockKey,
-		redsync.SetTries(RedisLockerTries),
-		redsync.SetRetryDelay(RedisLockerRetryDelay),
-		redsync.SetExpiry(RedisLockerExpiry),
+		redsync.SetTries(d.Option.RedisLockerTries),
+		redsync.SetRetryDelay(d.Option.RedisLockerRetryDelay),
+		redsync.SetExpiry(d.Option.RedisLockerExpiry),
 	)
 
 	for {
@@ -38,10 +76,10 @@ func SetCacheWithDistLock(pool *redis.Pool, key string, ttl interface{}, value i
 	}
 	defer mtx.Unlock()
 
-	_, err = redis.Bytes(pool.Get().Do("GET", key))
+	_, err = redis.Bytes(d.Pool.Get().Do("GET", key))
 
 	if err == redis.ErrNil {
-		_, err := pool.Get().Do("SETEX", key, ttl, value)
+		_, err := d.Pool.Get().Do("SETEX", key, ttl, value)
 		if err != nil {
 			return err
 		}
