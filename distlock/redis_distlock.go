0a2fa5c446aea25d6b008fd5e5854ff36b750cc3
Wahyu Said <bps.phi@gmail.com>
2019-11-27 13:20:27 +0700

    distlock with kmutex

--- distlock/redis_distlock.go
+++ distlock/redis_distlock.go
@@ -2,79 +2,36 @@ package distlock
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/go-redsync/redsync"
 	"github.com/gomodule/redigo/redis"
+	"github.com/im7mortal/kmutex"
 )
 
 type DistLock struct {
-	Pool   *redis.Pool
-	Option Option
+	Pool *redis.Pool
 }
-
-type Option struct {
-	RedisLockerTries      int
-	RedisLockerRetryDelay time.Duration
-	RedisLockerExpiry     time.Duration
-}
-
-const (
-	RedisLockerTries      = 3
-	RedisLockerRetryDelay = 2 * time.Second
-	RedisLockerExpiry     = 5 * time.Second
-)
 
 var pools []redsync.Pool
 var reds *redsync.Redsync
+var kMutex *kmutex.Kmutex
 
-func New(pool *redis.Pool, option ...Option) (distLock *DistLock) {
+func New(pool *redis.Pool) (distLock *DistLock) {
 	pools = []redsync.Pool{pool}
 	reds = redsync.New(pools)
+	kMutex = kmutex.New()
 
-	o := makeOption(option)
 	return &DistLock{
-		Pool:   pool,
-		Option: o,
+		Pool: pool,
 	}
-}
-
-func makeOption(option []Option) (o Option) {
-	o.RedisLockerTries = RedisLockerTries
-	o.RedisLockerRetryDelay = RedisLockerRetryDelay
-	o.RedisLockerExpiry = RedisLockerExpiry
-
-	for _, opt := range option {
-		if opt.RedisLockerTries != 0 {
-			o.RedisLockerTries = opt.RedisLockerTries
-		}
-		if opt.RedisLockerRetryDelay != 0 {
-			o.RedisLockerRetryDelay = opt.RedisLockerRetryDelay
-		}
-		if opt.RedisLockerExpiry != 0 {
-			o.RedisLockerExpiry = opt.RedisLockerExpiry
-		}
-	}
-	return o
 }
 
 func (d *DistLock) SetCacheWithDistLock(key string, ttl interface{}, value interface{}) (err error) {
 
 	lockKey := fmt.Sprintf("lock-key-%s", key)
 
-	mtx := reds.NewMutex(lockKey,
-		redsync.SetTries(d.Option.RedisLockerTries),
-		redsync.SetRetryDelay(d.Option.RedisLockerRetryDelay),
-		redsync.SetExpiry(d.Option.RedisLockerExpiry),
-	)
-
-	for {
-		errLock := mtx.Lock()
-		if errLock == nil {
-			break
-		}
-	}
-	defer mtx.Unlock()
+	kMutex.Lock(lockKey)
+	defer kMutex.Unlock(lockKey)
 
 	_, err = redis.Bytes(d.Pool.Get().Do("GET", key))
 
